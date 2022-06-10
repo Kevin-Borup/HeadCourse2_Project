ce460cccf29e6482789570c4333fa3db7fcf049a
Kevin Borup - Mobile <kevi1291@zbc.dk>
2022-06-10 09:32:14 +0200

    app.py setup

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, redirect, request, url_for
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+        return render_template('FrontPage.html', title="Home")
+
+@app.route('/ParkingLotMap')
+def parkingLotMap():
+        return render_template('ParkingLotMap.html', title="Parking Lot")
+
+@app.route('/Profile')
+def profile():
+    return render_template('Profile.html', title="Profile")
+
+@app.route('/LoginRegister')
+def loginRegister():
+    return render_template('LoginRegister.html', title="LoginRegister")
+
+
