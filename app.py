f6c420fc6aaa1229e718695963806f50c28bd00c
Kevin Borup - Mobile <kevi1291@zbc.dk>
2022-06-10 10:25:55 +0200

    venv environment

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, redirect, request, url_for
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def home():
         return render_template('FrontPage.html', title="Home")
 
--- static/js/CommonLoader.js
+++
@@ -1,5 +0,0 @@
-$(function () {
-    $("#banner").load("../common/banner.html");
-    $("#nav").load("../common/nav.html");
-    //$("#foot").load("common/footer.html");
-})
