e541d777a4a97aeee041600b9cbb222701096f33
MP-Hartvig <mip@hartvigconsult.dk>
2022-06-12 19:45:22 +0200

    Succeded in getting data from local test db to a html page

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from flask import Flask, render_template, redirect, request, url_for
+import psycopg2
 
 app = Flask(__name__)
 
+conn = psycopg2.connect("dbname=postgres user=postgres password=Kode1234!")
+
+cur = conn.cursor()
+
+cur.execute('SELECT name FROM public.ducks WHERE id = 69')
+
+testData = cur.fetchone()
+
+cur.close()
+conn.close()
+
 @app.route('/')
 def home():
-        return render_template('FrontPage.html', title="Home")
+        return render_template('FrontPage.html', title="Home", data=testData)
 
 @app.route('/ParkingLotMap')
 def parkingLotMap():
