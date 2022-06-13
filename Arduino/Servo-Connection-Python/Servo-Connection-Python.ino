cf72d7474e20e90dfb898dcee9b4c2bc6410d08a
Kevin Borup - Mobile <kevi1291@zbc.dk>
2022-06-13 14:58:14 +0200

    Servo

--- Arduino/Servo-Connection-Python/Servo-Connection-Python.ino
+++ Arduino/Servo-Connection-Python/Servo-Connection-Python.ino
@@ -1,16 +1,18 @@
 #include <Servo.h>
 Servo gate;
+int gateDegrees;
 
 void setup() {
   // put your setup code here, to run once:
   gate.attach(2);
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   if (Serial.available() > 0){
-    int gateDegrees = Serial.read();
-    Serial.write(gateDegrees);
+    gateDegrees = Serial.parseInt();
     gate.write(gateDegrees);
   }
+  delay(1);
 }
