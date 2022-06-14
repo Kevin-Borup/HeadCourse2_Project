902eca0ad9aeda44a7c3ff9b8f62503469612659
MP-Hartvig <mip@hartvigconsult.dk>
2022-06-14 10:36:01 +0200

    Made methods to call the stored procedures

--- static/py/ProcessManagement.py
+++ static/py/ProcessManagement.py
@@ -1,5 +1,31 @@
+from static.py.dbConfig import config
+import psycopg2
+
 class ProcessManagement:
     __parkinglots = None
 
     def __init__(self, parkinglots):
         self.__parkinglots = parkinglots
+
+async def AddGateRecord(sectorId, gateTimestamp, userLicenseplate, enteredParkinglot):
+        
+        conn = None
+
+        newGateRecord = None
+
+        try:
+                params = await config()
+                conn = await psycopg2.connect(**params)
+                cur = await conn.cursor()
+                await cur.execute('CALL addgaterecord(%s,%s,%s,%s)', sectorId, gateTimestamp, userLicenseplate, enteredParkinglot)
+                await cur.execute('SELECT * FROM public.gate WHERE licenseplate = {userLicenseplate}')
+                newGateRecord = await cur.fetchone() # Fetches a single row from the database
+                await cur.close()
+
+        except (Exception, psycopg2.DatabaseError) as error:
+                print(error)
+
+        finally:
+                if conn is not None:
+                        await conn.close()
+                return newGateRecord
--- static/py/User.py
+++ static/py/User.py
@@ -1,3 +1,6 @@
+from static.py.dbConfig import config
+import psycopg2
+
 class User:
     __name = None
     __licensePlate = None
@@ -9,3 +12,26 @@ class User:
         self.__licensePlate = licensePlate
         self.__role = role
         self.__creditcard = creditcard
+
+async def AddUser(accName, accRole, accLicenseplate, accCardnumber):
+    
+        conn = None
+
+        createdUser = None
+
+        try:
+                params = await config()
+                conn = await psycopg2.connect(**params)
+                cur = await conn.cursor()
+                await cur.execute('CALL createaccount(%s,%s,%s,%s)', accName, accRole, accLicenseplate, accCardnumber)
+                await cur.execute('SELECT * FROM public.user_account WHERE cardnumber = {userCardnumber}')
+                createdUser = await cur.fetchone() # Fetches a single row from the database
+                await cur.close()
+
+        except (Exception, psycopg2.DatabaseError) as error:
+                print(error)
+
+        finally:
+                if conn is not None:
+                        await conn.close()
+                return createdUser
