a9a46964fd2e6703ad518a11e5e6a7a936ae6dd8
MP-Hartvig <mip@hartvigconsult.dk>
2022-06-15 10:53:08 +0200

    Fixed the frontpage with real data

--- app.py
+++ app.py
@@ -1,12 +1,36 @@
 from flask import Flask, render_template
 from static.py.dbConfig import config
-import psycopg2
-
+from static.py.ParkingLot import GetSectorsData, AddCarToParkingLot, RemoveCarFromParkingLot
 app = Flask(__name__)
 
+sector1 = 1
+sector2 = 2
+sector3 = 3
+
 @app.route('/')
-def home():
-        return render_template('FrontPage.html', title="Home", data=getTestName())
+async def home():
+
+        sector1Data = GetSectorsData(sector1)
+        (sector1Id, s1Total, s1AS, s1US) = sector1Data
+
+        sector2Data = GetSectorsData(sector2)
+        (sector2Id, s2Total, s2AS, s2US) = sector2Data
+
+        sector3Data = GetSectorsData(sector3)
+        (sector3Id, s3Total, s3AS, s3US) = sector3Data
+
+        return render_template(
+                'FrontPage.html',
+                title ="Home",
+                sector1Total = s1Total,
+                sector1AS = s1AS, 
+                sector1US = s1US,
+                sector2Total = s2Total,
+                sector2AS = s2AS,
+                sector2US = s2US,
+                sector3Total = s3Total,
+                sector3AS = s3AS,
+                sector3US = s3US)
 
 @app.route('/ParkingLotMap')
 def parkingLotMap():
@@ -24,21 +48,11 @@ def login():
 def register():
     return render_template('RegisterPage.html', title="Register")
 
-def getTestName():
-        conn = None
-        testData = None
-        try:
-                params = config()
-                conn = psycopg2.connect(**params)
-                cur = conn.cursor()
-                cur.execute('SELECT * FROM public.user_account WHERE id = 1')
-                testData = cur.fetchone() # Fetches a single row from the database
-                cur.close()
+def convertTuple(tup: tuple):
+    # initialize an empty string
+    str = ''.join()
+    return str
 
-        except (Exception, psycopg2.DatabaseError) as error:
-                print(error)
-
-        finally:
-                if conn is not None:
-                        conn.close()
-                return testData
+def replaceSymbols(someString: str):
+        someString.strip(('(),'))
+        return someString
