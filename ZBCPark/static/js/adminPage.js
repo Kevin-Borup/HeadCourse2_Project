361cade57688f84249761e30b1fa8e9d7e7af222
MP-Hartvig <mip@hartvigconsult.dk>
2022-06-16 12:07:35 +0200

    .

--- ZBCPark/static/js/adminPage.js
+++ ZBCPark/static/js/adminPage.js
@@ -1,2 +1,28 @@
 const adminButton = document.getElementById("#adminButton")
 adminButton.style.visibility = "visible";
+
+function checkLogin() {
+
+    const email = document.getElementById("emailInput").value
+    const password = document.getElementById("passwordInput").value
+
+    $.ajax({
+        type: "GET",
+        url: "",
+        data: { param: email, password },
+        success: callbackFunction
+    })
+}
+
+function callbackFunction(response) {
+    if(response != null) {
+        document.cookie = response // userId
+        window.location.href = "/ProfilePage";
+    }
+    else {
+        alert("Wrong email or password")
+    }
+}
+
+// var xhttp = new XMLHttpRequest();
+// xhttp.open(GET, serverURL, true)
